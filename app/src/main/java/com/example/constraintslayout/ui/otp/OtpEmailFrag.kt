fac8e505eff35c6a6b95eb36d2d302f7156504a4
Puneet Kumar <puneet.kumar@geminisolutions.in>
2019-10-01 13:15:31 +0530

    implemnted otp screen

--- app/src/main/java/com/example/constraintslayout/ui/otp/OtpEmailFrag.kt
+++ app/src/main/java/com/example/constraintslayout/ui/otp/OtpEmailFrag.kt
@@ -6,11 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.constraintslayout.R
+import com.example.constraintslayout.ui.mainActivity.MainActivity
+import kotlinx.android.synthetic.main.fragment_otp.*
 
 class OtpEmailFrag : Fragment()
 {
+
+    // ==================================================================================//
+    // MARK: ********************* Variable *********************
+    // ==================================================================================//
+
+
+    // ==================================================================================//
+    // MARK: ********************* Default  Fragment Method *********************
+    // ==================================================================================//
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_otp_email,container,false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        setListner()
+
+    }
+
+    // ==================================================================================//
+    // MARK: ********************* Listener *********************
+    // ==================================================================================//
+
+
+    fun setListner() {
+
+        send_otp.setOnClickListener {
+
+            (activity as MainActivity).addFragment(OtpFrag(), "")
+        }
+    }
+
+
 }
